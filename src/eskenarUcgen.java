c6fbabc7b91b1c37b8451cc2465a4fe8e59d3a0f
ProtonMe <keremtopuzogullari@gmail.com>
2025-10-11 20:39:17 +0300

    Create test.java

---
+++ src/eskenarUcgen.java
@@ -0,0 +1,11 @@
+public class eskenarUcgen {
+    public static void main(String[] args) {
+        System.out.println("Eskenar Üçgen Hesaplamaları");
+        double kenar = 5.0;
+        double yukseklik = Math.sqrt(kenar * kenar - (kenar / 2) * (kenar / 2));
+        double alan = (kenar * yukseklik) / 2;
+        double cevre = kenar * 3;
+        System.out.println("Üçgenin Alanı: " + alan);
+        System.out.println("Üçgenin Çevresi: " + cevre);
+    }
+}
