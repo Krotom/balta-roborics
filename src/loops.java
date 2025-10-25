c5b1f5e5b39f6805f2c65374f1d93456705ea26f
ProtonMe <keremtopuzogullari@gmail.com>
2025-10-25 20:45:59 +0300

    Increase random number range to 1000 in loops.java

--- src/loops.java
+++ src/loops.java
@@ -5,7 +5,7 @@ void main() {
     Integer[] numbers = new Integer[50];
 
     for (int i = 0; i < 50; i++) {
-        numbers[i] = random.nextInt(100);
+        numbers[i] = random.nextInt(1000);
     }
 
     Integer[] sortedNumbers = Arrays.copyOf(numbers, numbers.length);
