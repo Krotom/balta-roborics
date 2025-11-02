ca7b20097ee13d7406f0e777f63f1db518a0e9b7
Kerem Topuzoğulları <122735384+Krotom@users.noreply.github.com>
2025-11-02 21:30:48 +0300

    Add bubble sort implementation to loops.java
    Implemented bubble sort algorithm and added output for comparison.

--- src/loops.java
+++ src/loops.java
@@ -1,5 +1,23 @@
 import java.util.Random;
 
+void bubble_sort(int[] arr) {
+	int n = arr.length;
+	for (int i = 0; i < n; i++) {
+		boolean swap = false;
+		for (int j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j+1]) {
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				swap = true;
+			}
+		}
+		if (!swap) {
+			break;
+		}
+	}
+}
+
 void main() {
     Random random = new Random();
     Integer[] numbers = new Integer[50];
@@ -10,6 +28,13 @@ void main() {
 
     Integer[] sortedNumbers = Arrays.copyOf(numbers, numbers.length);
     Arrays.sort(sortedNumbers);
+	
+    IO.println("Arrays.sort: " + Arrays.toString(sortedNumbers));
 
-    IO.println(Arrays.toString(sortedNumbers));
+	Integer[] sortedNumbers2 = Arrays.copyOf(numbers, numbers.length);
+    bubble_sort(sortedNumbers2);
+
+    IO.println("Bubble Sort Algoritması: " + Arrays.toString(sortedNumbers2));
+
+	IO.println("Arrayler aynı mı: " + Arrays.equals(sortedNumbers1, sortedNumbers2));
 }
