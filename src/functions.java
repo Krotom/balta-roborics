7853921330ad62cb5a91a558dc4b7bce187a718e
ProtonMe <keremtopuzogullari@gmail.com>
2025-11-15 13:26:15 +0300

    Update main to analyze primes and perfect numbers

--- src/functions.java
+++ src/functions.java
@@ -1,73 +1,46 @@
 void main() {
-    int oddSum = sumOfOddNumbers(100);
-    IO.println("Sum of all odd numbers from 1 to 100: " + oddSum);
-
-    int evenSum = sumOfEvenNumbers(100);
-    IO.println("Sum of all even numbers from 1 to 100: " + evenSum);
-
-    int factorial = calculateFactorial(5);
-    IO.println("Factorial of 5: " + factorial);
-
-    int multiplesOf3 = sumOfMultiples(100, 3);
-    IO.println("Sum of multiples of 3 from 1 to 100: " + multiplesOf3);
-
-    int primeCount = countPrimes(50);
-    IO.println("Number of prime numbers from 1 to 50: " + primeCount);
-
-    int fibSum = sumOfFibonacci(10);
-    IO.println("Sum of first 10 Fibonacci numbers: " + fibSum);
-
-    int squaresSum = sumOfSquares(10);
-    IO.println("Sum of squares from 1 to 10: " + squaresSum);
-
-    boolean isPerfect = isPerfectNumber(28);
-    IO.println("Is 28 a perfect number? " + isPerfect);
-}
-
-int sumOfOddNumbers(int limit) {
-    int sum = 0;
-    for (int i = 1; i <= limit; i++) {
-        if (i % 2 != 0) {
-            sum += i;
+    for (int num = 1; num <= 30; num++) {
+        if (isPrime(num)) {
+            int digitSum = sumOfDigits(num);
+            IO.println("Prime: " + num + " | Digit Sum: " + digitSum + " | Is Perfect: " + isPerfect(digitSum));
         }
     }
-    return sum;
-}
 
-int sumOfEvenNumbers(int limit) {
-    int sum = 0;
-    for (int i = 1; i <= limit; i++) {
-        if (i % 2 == 0) {
-            sum += i;
+    IO.println("\n=== Checking Numbers 1-100 ===\n");
+
+    for (int i = 1; i <= 100; i++) {
+        int digits = sumOfDigits(i);
+
+        if (isPrime(digits)) {
+            IO.println(i + " has prime digit sum: " + digits);
+        }
+
+        if (isPerfect(i)) {
+            IO.println(i + " is a perfect number! Digit sum: " + digits);
         }
     }
-    return sum;
-}
 
-int calculateFactorial(int n) {
-    int result = 1;
-    for (int i = 1; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
+    IO.println("\n=== Special Number Combinations ===\n");
 
-int sumOfMultiples(int limit, int multiple) {
-    int sum = 0;
-    for (int i = multiple; i <= limit; i += multiple) {
-        sum += i;
-    }
-    return sum;
-}
-
-int countPrimes(int limit) {
     int count = 0;
-    for (int i = 2; i <= limit; i++) {
-        if (isPrime(i)) {
+    for (int n = 1; n <= 200; n++) {
+        int digitSum = sumOfDigits(n);
+        if (isPrime(n) && isPrime(digitSum)) {
             count++;
+            IO.println("Double Prime: " + n + " (digit sum: " + digitSum + ")");
         }
     }
-    return count;
+
+    IO.println("\nTotal double primes found: " + count);
+}
+
+int sumOfDigits(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
 }
 
 boolean isPrime(int n) {
@@ -83,28 +56,8 @@ boolean isPrime(int n) {
     return true;
 }
 
-int sumOfFibonacci(int n) {
-    int sum = 0;
-    int a = 0, b = 1;
-
-    for (int i = 0; i < n; i++) {
-        sum += a;
-        int temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return sum;
-}
-
-int sumOfSquares(int n) {
-    int sum = 0;
-    for (int i = 1; i <= n; i++) {
-        sum += i * i;
-    }
-    return sum;
-}
-
-boolean isPerfectNumber(int n) {
+boolean isPerfect(int n) {
+    if (n <= 1) return false;
     int sum = 0;
     for (int i = 1; i < n; i++) {
         if (n % i == 0) {
